97e9f6a708526e7276f5fc84bf81aba8ba05be03
BobaFett1 <michelehmen@hotmail.de>
2016-12-23 11:30:02 +0100

    fixed status code issues on /signup

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -65,9 +65,9 @@ module.exports = function(app, passport) {
         res.render('connect-local.ejs', { message: req.flash('loginMessage') });
     });
     app.post('/connect/local', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
+        // successRedirect : '/profile', // redirect to the secure profile section
+        // failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+        // failureFlash : true // allow flash messages
     }));
 
 
