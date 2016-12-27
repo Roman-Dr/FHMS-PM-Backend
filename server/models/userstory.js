6ccd97369db8ed6a432c11e2cffbe3ba2ccebfe2
Roman-Dr <Roman Drobjazko>
2016-12-27 21:00:18 +0100

    User story little fixes

--- server/models/userstory.js
+++ server/models/userstory.js
@@ -1,21 +1,15 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
-//var autoIncrement = require('mongoose-auto-increment');
-
-//autoIncrement.initialize(db);
 
 var userStorySchema = new schema({
+        _id: Number,
         title: String,
-        //type: schema.ObjectId,
-        //ref: 'users',
+        autor: String,
         complete: Boolean,
         timestmp: Date
     },
     {versionKey: false} // DISABLE VERSIONING (_v)
 );
-
-//userStorySchema.plugin(autoIncrement.plugin, 'userStory');
-//var userStory = db.model('userStory', userStorySchema);
 
 userStorySchema.methods.displayTitle = function (cb) {
     return this.title;
