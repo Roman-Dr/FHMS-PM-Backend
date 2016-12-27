61f2ad2062f061030cdda460c90bad8829e6f132
Roman-Dr <Roman Drobjazko>
2016-12-27 14:33:52 +0100

    userStory (update and delete)

--- server/routes/userstorys.js
+++ server/routes/userstorys.js
@@ -4,16 +4,16 @@ var router = express.Router();
 var UserStory = require('./../models/userstory');
 
 
-router.route('/userstorys')
+router.route('/userstories')
 //
 // GET ALL USERSSTORYS
 //
     .get(function (req, res) {
-        UserStory.find(function (err, userstorys) {
+        UserStory.find(function (err, userstories) {
             if (err)
                 res.send(err);
 
-            res.json(userstorys);
+            res.json(userstories);
         });
     })
     //
@@ -34,6 +34,57 @@ router.route('/userstorys')
             res.json({ message: 'New story ' + newUserStory.displayTitle() + ' was created!', data: newUserStory });
         });
     });
+
+
+
+router.route('/userstory/:id')
+//
+// GET SINGLE USERSSTORY
+//
+    .get(function (req, res) {
+        User.findById(req.params.id, function (err, userstory) {
+            if (err)
+                res.send(err);
+
+            res.json(userstory);
+        });
+    })
+    //
+    // UPDATE SINGLE USERSSTORY
+    //
+    .put(function (req, res) {
+        User.findById(req.params.id,
+            function (err, userstory) {
+                if (err) {
+                    console.error(err);
+
+                    return res.sendStatus(404);
+                }
+                newUserStory.title = req.body.title;
+                newUserStory.autor = req.body.autor;
+                newUserStory.complete = req.body.complete;
+                newUserStory.timestmp = req.body.timestmp;
+
+                user.save(function (err) {
+                    if (err)
+                        res.send(err);
+
+                    res.json({ message: 'Userstory was updated!', data: userstory });
+                });
+            });
+    })
+    //
+    // DELETE SINGLE USERSSTORY
+    //
+    .delete(function (req, res) {
+        User.findByIdAndRemove(req.params.id,
+            function (deleteErr, deleteRes) {
+                if (deleteErr) return console.error(deleteErr);
+            });
+
+        return res.json(200);
+    });
+
 
 
 
