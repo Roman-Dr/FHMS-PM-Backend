4122d18393924351afe2a3feca82a590e2790eaa
BobaFett1 <michelehmen@hotmail.de>
2017-01-10 18:45:07 +0100

    Fixed issue

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.0:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:4200');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
