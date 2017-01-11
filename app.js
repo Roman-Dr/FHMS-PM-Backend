df5fbb66729a3e6ce9ad4496a0483d2ad1379ec2
David Melcher <dm070491@fh-muenster.de>
2017-01-11 18:22:39 +0100

    TEST

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ app.set('view engine', 'ejs');
 
 var corsOptions = {
     origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204,
+    credentials: true
 };
 
 app.options('*', cors(corsOptions));
