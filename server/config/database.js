6e1ea6b6bbe2793fa85d28afc90a443ee1e06d94
Roman-Dr <Roman Drobjazko>
2017-04-02 15:41:48 +0200

    change db url

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://localhost/test' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://Admin:Nimda@ds060369.mlab.com:60369/scrumjs' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
